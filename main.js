9d0c0111292b649d4f2a94a2d49f3c69cd6ca79e
JOJOK63 <krariajordan@gmail.com>
2023-08-26 23:15:40 +0200

    modification for netlif

--- main.js
+++ main.js
@@ -1,7 +1,112 @@
 import './style.scss'
 
 const grid = document.querySelector('.js-grid-time-tracking')
-var data_json = "";
+// modification for netlify 
+var   data_json=[
+  {
+    "title": "Work",
+    "timeframes": {
+      "daily": {
+        "current": 5,
+        "previous": 7
+      },
+      "weekly": {
+        "current": 32,
+        "previous": 36
+      },
+      "monthly": {
+        "current": 103,
+        "previous": 128
+      }
+    }
+  },
+  {
+    "title": "Play",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 2
+      },
+      "weekly": {
+        "current": 10,
+        "previous": 8
+      },
+      "monthly": {
+        "current": 23,
+        "previous": 29
+      }
+    }
+  },
+  {
+    "title": "Study",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 7
+      },
+      "monthly": {
+        "current": 13,
+        "previous": 19
+      }
+    }
+  },
+  {
+    "title": "Exercise",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 4,
+        "previous": 5
+      },
+      "monthly": {
+        "current": 11,
+        "previous": 18
+      }
+    }
+  },
+  {
+    "title": "Social",
+    "timeframes": {
+      "daily": {
+        "current": 1,
+        "previous": 3
+      },
+      "weekly": {
+        "current": 5,
+        "previous": 10
+      },
+      "monthly": {
+        "current": 21,
+        "previous": 23
+      }
+    }
+  },
+  {
+    "title": "Self Care",
+    "timeframes": {
+      "daily": {
+        "current": 0,
+        "previous": 1
+      },
+      "weekly": {
+        "current": 2,
+        "previous": 2
+      },
+      "monthly": {
+        "current": 7,
+        "previous": 11
+      }
+    }
+  }
+]
+
 fetch('./assets/data.json')
   .then(response => {
     if (!response.ok) {
@@ -10,12 +115,13 @@ fetch('./assets/data.json')
     return response.json();
   })
   .then(data => {
-    createElementHtml(data)
-    data_json = data;
+    createElementHtml(data_json)
+   // data_json = data;
   })
   .catch(error => {
     console.error('Une erreur s\'est produite:', error);
   });
+
 
 
   function createElementHtml(data,data_value){
